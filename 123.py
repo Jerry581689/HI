98b58c00e9618455e02019fbaa5fd74111b6043f
jerry <Jerry581689@gmail.com>
2023-07-25 17:12:22 +0800

    temp

--- 123.py
+++ 123.py
@@ -1,6 +1,14 @@
-x = 5
-name = input("輸入名字 : ")
-print(x)
-price = 10
-for  i in range(5):
-	print(name , price , "塊")
+# x = 5
+# name = input("輸入名字 : ")
+# number = int(input("$ : "))
+# print(x)
+# price = 10
+# for  i in range(5):
+# 	print(name , number , "塊")
+# 	if name == "mom" :
+# 		print("口憐")
+
+
+temC = float(input("輸入溫度 C : "))
+res = temC / 5 * 9 + 32
+print('F : ' , res)
